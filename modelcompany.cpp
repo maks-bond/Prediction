c93b28f1fe0aec7d2233b4f776b3e4f627f281cf
Vlad Grytsun <vlg92@mail.ru>
2013-04-03 14:40:51 +0300

    modelcompany

--- modelcompany.cpp
+++ modelcompany.cpp
@@ -17,13 +17,32 @@ ModelCompany::~ModelCompany()
 
 void ModelCompany::AddItem(ModelItem* i_model_item)
 {
-    m_model.append(i_model_item);
-    m_object_model.append(i_model_item);
+
+    QDate cur_date = QDate::fromString(i_model_item->date(),"yyyy-M-d");
+
+    if(cur_date.isValid()){
+        m_model.append(i_model_item);
+        m_object_model.append(i_model_item);
+
+        if(!m_period_start.isValid() || cur_date < m_period_start)
+            m_period_start = cur_date;
+
+        if(!m_period_end.isValid() || cur_date > m_period_end)
+            m_period_end = cur_date;
+    }
 }
 
 void ModelCompany::SetCompanyName(const QString &i_company_name)
 {
     m_company_name = i_company_name;
+}
+
+QString ModelCompany::GetCompanyName() const
+{
+    if(m_company_name.isEmpty())
+        return m_company_name;
+    else
+        return NULL;
 }
 
 QList<ModelItem*> ModelCompany::GetListOfItems() const
@@ -34,4 +53,14 @@ QList<ModelItem*> ModelCompany::GetListOfItems() const
 QList<QObject*> ModelCompany::GetListOfObjectItems() const
 {
     return m_object_model;
+}
+
+QDate ModelCompany::getStartPeriod() const
+{
+    return m_period_start;
+}
+
+QDate ModelCompany::getEndPeriod() const
+{
+    return m_period_end;
 }
--- modelcompany.h
+++ modelcompany.h
@@ -3,6 +3,7 @@
 
 #include <QObject>
 #include <QList>
+#include <QDate>
 
 class ModelItem;
 
@@ -15,11 +16,18 @@ public:
 
     void AddItem(ModelItem* i_model_item);
     void SetCompanyName(const QString& i_company_name);
+    QString GetCompanyName() const;
 
     QList<ModelItem*> GetListOfItems() const;
     QList<QObject*> GetListOfObjectItems() const;
 
+    QDate getStartPeriod() const;
+    QDate getEndPeriod() const;
+
 private:
+    QDate m_period_start;
+    QDate m_period_end;
+
     QString m_company_name;
     QList<ModelItem*> m_model;
     QList<QObject*> m_object_model;
