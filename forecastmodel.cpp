2f5f5dcffe89620effa49c4a56e98bf3d16ff4dd
Vlad Grytsun <vlg92@mail.ru>
2013-04-08 22:52:00 +0300

    update ForecastModel
    added few methods

--- forecastmodel.cpp
+++ forecastmodel.cpp
@@ -1,5 +1,52 @@
 #include "forecastmodel.h"
 
 ForecastModel::ForecastModel()
+    : m_is_comptuted(false), m_quality(-1)
 {
+
+}
+
+ForecastModel::ForecastModel(QVector<int> &i_a)
+    : m_is_comptuted(false), m_a(i_a), m_quality(-1)
+{
+
+}
+
+double ForecastModel::Evaluate(const QVector<double> i_a)
+{
+    if(!m_is_comptuted) _Compute();
+    double res = 0;
+
+    for(int j=0;j<i_a.size();j++) res+= i_a[j]*m_w[j];
+
+    return res;
+}
+
+
+void ForecastModel::_Compute()
+{
+    if(m_is_comptuted) return;
+
+    //implement
+
+    m_is_comptuted = true;
+}
+
+
+double ForecastModel::Quality()
+{
+    if(m_quality != -1) return m_quality;
+    if(!m_is_comptuted) _Compute();
+
+    m_quality = 0;
+    // implement
+
+    return m_quality;
+}
+
+void ForecastModel::SetParams(QVector<int>& i_a)
+{
+    if(m_a == i_a) return;
+    m_a = i_a;
+    m_is_comptuted = false;
 }
--- forecastmodel.h
+++ forecastmodel.h
@@ -8,9 +8,20 @@ class ForecastModel
 
 public:
     ForecastModel();
+    ForecastModel(QVector<int>& i_a);
+
+    double Evaluate(const QVector<double> i_a);
+    double Quality();
+
+    void SetParams(QVector<int>& i_a);
 
 private :
+    void _Compute();
+
     bool m_is_comptuted;
+    double m_quality;
+    QVector<double> m_w; // factors in regression model
+    QVector<int> m_a; // variables in regression model
 
 };
 
